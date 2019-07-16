450b04d75efa75cfcb1d593c051bfb4832baffe0
nunttse62525 <ngtuyetnu1997@gmail.com>
2019-07-16 19:24:44 +0700

    CORS

--- src/main/java/com/fu/aws/blogwebsite/BlogWebsiteApplication.java
+++ src/main/java/com/fu/aws/blogwebsite/BlogWebsiteApplication.java
@@ -16,4 +16,14 @@ public class BlogWebsiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogWebsiteApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
